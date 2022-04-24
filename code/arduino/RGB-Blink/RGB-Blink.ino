905dc17eb7302dbb03a8674ae42b29a26cab67e2
Sukesh <sukesh@live.com>
2022-04-25 02:04:45 +0530

    RGB-Blink sample for all 3 boards

--- code/arduino/RGB-Blink/RGB-Blink.ino
+++ code/arduino/RGB-Blink/RGB-Blink.ino
@@ -1,10 +1,27 @@
 #include <FastLED.h>
 
+#define TINY_S3
+//#define FEATHER_S3
+//#define PRO_S3
+
+#ifdef TINY_S3
+  #define DATA_PIN 18
+  #define POWER_PIN 17
+#endif
+
+#ifdef FEATHER_S3
+  #define DATA_PIN 40
+  //#define POWER_PIN 17
+#endif
+
+#ifdef PRO_S3
+  #define DATA_PIN 18
+  //#define POWER_PIN 17
+#endif
+
+
 // How many leds in your strip?
 #define NUM_LEDS 1
-
-#define DATA_PIN 18
-#define POWER_PIN 17
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
@@ -13,9 +30,11 @@ void setup() {
 
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
 
+#ifdef TINY_S3
   // Enable LED
   pinMode(POWER_PIN, OUTPUT);
   digitalWrite(POWER_PIN, HIGH);
+#endif
 }
 
 void loop() { 
